0940778f7affafab3b3e2f224ac68b6f9d803f33
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-09-01 20:04:04 +0000

    use copilot to update the GenerateSalesData method using inline chat

--- APL2007M3SalesReport-InlineChat/Program.cs
+++ APL2007M3SalesReport-InlineChat/Program.cs
@@ -25,9 +25,32 @@ namespace ReportGenerator
         {
             public DateOnly dateSold;
             public string departmentName;
-            public int productID;
+            public string productID;
             public int quantitySold;
             public double unitPrice;
+            public double baseCost;
+            public int volumeDiscount;
+        }
+        
+        /// <summary>
+        /// Represents the product departments.
+        /// </summary>
+        public struct ProdDepartments
+        {
+            /// <summary>
+            /// Gets the names of the departments.
+            /// </summary>
+            public static string[] DepartmentNames = { "Menswear", "Womenswear", "Childrenswear", "Footwear", "Accessories", "Sportswear", "Outerwear", "Intimates" };
+
+            /// <summary>
+            /// Gets the abbreviations of the departments.
+            /// </summary>
+            public static string[] DepartmentAbbreviations = { "MENS", "WMNS", "CHLD", "FOOT", "ACCS", "SPORT", "OUTR", "INTM" };
+        }
+
+        public struct ManufacturingSites
+        {
+            public static string[] manSites = { "US1", "US2", "UK1", "UK2", "FR1", "FR2", "DE1", "DE2", "JP1", "JP2" };
         }
 
         /* the GenerateSalesData method returns 1000 SalesData records. It assigns random values to each field of the data structure */
@@ -39,10 +62,24 @@ namespace ReportGenerator
             for (int i = 0; i < 1000; i++)
             {
                 salesData[i].dateSold = new DateOnly(2023, random.Next(1, 13), random.Next(1, 29));
-                salesData[i].departmentName = "Department " + random.Next(1, 11);
-                salesData[i].productID = random.Next(1, 101);
+                salesData[i].departmentName = ProdDepartments.DepartmentNames[random.Next(0, ProdDepartments.DepartmentNames.Length)];
+
+                int indexOfDept = Array.IndexOf(ProdDepartments.DepartmentNames, salesData[i].departmentName);
+                string deptAbb = ProdDepartments.DepartmentAbbreviations[indexOfDept];
+                string firstDigit = (indexOfDept + 1).ToString();
+                string nextTwoDigits = random.Next(1, 100).ToString("D2");
+                sizeCode = new string[] { "XS", "S", "M", "L", "XL" }[random.Next(0, 5)];
+                string[] colorCodes = { "BK", "BL", "GR", "RD", "YL", "OR", "WT", "GY" };
+                string colorCode = colorCodes[random.Next(0, colorCodes.Length)];
+                string[] manufacturingSites = ManufacturingSites.manSites;
+                int randomIndex = random.Next(0, manufacturingSites.Length);
+                string manufacturingSite = manufacturingSites[randomIndex];
+
+                salesData[i].productID = $"{deptAbb}-{firstDigit}{nextTwoDigits}-{sizeCode}-{colorCode}-{manufacturingSite}";
                 salesData[i].quantitySold = random.Next(1, 101);
-                salesData[i].unitPrice = random.Next(1, 101) + random.NextDouble();
+                salesData[i].unitPrice = random.Next(25, 300) + random.NextDouble();
+                salesData[i].baseCost = salesData[i].unitPrice * (1 - (random.Next(5, 21) / 100.0));
+                salesData[i].volumeDiscount = (int)(salesData[i].quantitySold * 0.1);
             }
 
             return salesData;
