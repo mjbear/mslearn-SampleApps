d8970002164c8a96b5be0d62e496cf60bd109efe
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-09-02 03:39:35 +0000

    Create unit tests using GitHub Copilot - section1

--- APL2007M4PrimeService/PrimeService.UnitTests/PrimeServiceTests.cs
+++ APL2007M4PrimeService/PrimeService.UnitTests/PrimeServiceTests.cs
@@ -10,44 +10,26 @@ public class PrimeServiceTests
     }
 
     [Fact]
-    public void IsPrime_InputIs1_ReturnsFalse()
+    public void IsPrime_InputIs1_ReturnFalse()
     {
         var result = _primeService.IsPrime(1);
+
         Assert.False(result, "1 should not be prime");
     }
 
     [Fact]
-    public void IsPrime_InputIs2_ReturnsTrue()
-    {
-        var result = _primeService.IsPrime(2);
-        Assert.True(result, "2 should be prime");
-    }
-
-    [Fact]
-    public void IsPrime_InputIs3_ReturnsTrue()
-    {
-        var result = _primeService.IsPrime(3);
-        Assert.True(result, "3 should be prime");
-    }
-
-    [Fact]
-    public void IsPrime_InputIs4_ReturnsFalse()
+    public void IsPrime_InputIs4_ReturnFalse()
     {
         var result = _primeService.IsPrime(4);
+
         Assert.False(result, "4 should not be prime");
     }
 
     [Fact]
-    public void IsPrime_InputIs17_ReturnsTrue()
+    public void IsPrime_InputIs5_ReturnTrue()
     {
-        var result = _primeService.IsPrime(17);
-        Assert.True(result, "17 should be prime");
-    }
+        var result = _primeService.IsPrime(5);
 
-    [Fact]
-    public void IsPrime_InputIs18_ReturnsFalse()
-    {
-        var result = _primeService.IsPrime(18);
-        Assert.False(result, "18 should not be prime");
+        Assert.True(result, "5 should be prime");
     }
 }
