491356b1fd8de1956e1ebe88e31f867f0c780dab
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-09-07 23:26:06 +0000

    Use GitHub Copilot to refactor the EnumHelper class

--- AccelerateDevGitHubCopilot/src/Library.ApplicationCore/Enums/EnumHelper.cs
+++ AccelerateDevGitHubCopilot/src/Library.ApplicationCore/Enums/EnumHelper.cs
@@ -1,27 +1,57 @@
-﻿using System.ComponentModel;
-using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
-namespace Library.ApplicationCore.Enums;
-
-public static class EnumHelper
+namespace Library.ApplicationCore.Enums
 {
-    public static string GetDescription(Enum value)
+    public static class EnumHelper
     {
-        if (value == null)
-            return string.Empty;
+        private static readonly Dictionary<LoanExtensionStatus, string> LoanExtensionStatusDescriptions;
+        private static readonly Dictionary<LoanReturnStatus, string> LoanReturnStatusDescriptions;
+        private static readonly Dictionary<MembershipRenewalStatus, string> MembershipRenewalStatusDescriptions;
 
-        FieldInfo fieldInfo = value.GetType().GetField(value.ToString())!;
-
-        DescriptionAttribute[] attributes =
-            (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-        if (attributes != null && attributes.Length > 0)
+        static EnumHelper()
         {
-            return attributes[0].Description;
+            LoanExtensionStatusDescriptions = new Dictionary<LoanExtensionStatus, string>
+            {
+                { LoanExtensionStatus.Success, "Book loan extension was successful." },
+                { LoanExtensionStatus.LoanNotFound, "Loan not found." },
+                { LoanExtensionStatus.LoanExpired, "Cannot extend book loan as it already has expired. Return the book instead." },
+                { LoanExtensionStatus.MembershipExpired, "Cannot extend book loan due to expired patron's membership." },
+                { LoanExtensionStatus.LoanReturned, "Cannot extend book loan as the book is already returned." },
+                { LoanExtensionStatus.Error, "Cannot extend book loan due to an error." }
+            };
+
+            LoanReturnStatusDescriptions = new Dictionary<LoanReturnStatus, string>
+            {
+                { LoanReturnStatus.Success, "Book was successfully returned." },
+                { LoanReturnStatus.LoanNotFound, "Loan not found." },
+                { LoanReturnStatus.AlreadyReturned, "Cannot return book as the book is already returned." },
+                { LoanReturnStatus.Error, "Cannot return book due to an error." }
+            };
+
+            MembershipRenewalStatusDescriptions = new Dictionary<MembershipRenewalStatus, string>
+            {
+                { MembershipRenewalStatus.Success, "Membership renewal was successful." },
+                { MembershipRenewalStatus.PatronNotFound, "Patron not found." },
+                { MembershipRenewalStatus.TooEarlyToRenew, "It is too early to renew the membership." },
+                { MembershipRenewalStatus.LoanNotReturned, "Cannot renew membership due to an outstanding loan." },
+                { MembershipRenewalStatus.Error, "Cannot renew membership due to an error." }
+            };
         }
-        else
+
+        public static string GetDescription(Enum value)
         {
-            return value.ToString();
+            if (value == null)
+                return string.Empty;
+
+            return value switch
+            {
+                LoanExtensionStatus loanExtensionStatus => LoanExtensionStatusDescriptions[loanExtensionStatus],
+                LoanReturnStatus loanReturnStatus => LoanReturnStatusDescriptions[loanReturnStatus],
+                MembershipRenewalStatus membershipRenewalStatus => MembershipRenewalStatusDescriptions[membershipRenewalStatus],
+                _ => value.ToString()
+            };
         }
     }
 }
